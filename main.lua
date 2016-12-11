080927c0cf010affe572adff327270c524389a7e
Corey Kruger <Corey Kruger>
2016-12-11 11:31:32 -0700

    applied new physics

--- main.lua
+++ main.lua
@@ -58,28 +58,37 @@ function love.load()
   background:drawCanvas()
 
   block = Block:new(world, 25, 25, 50, 50)
+  block.body:setFixedRotation(true)
   block2 = Block:new(world, 225, 25, 50, 50)
 
   ceiling = Wall:new(world, 325, 1, 650, 1)
   ground = Wall:new(world, 325, 649, 650, 1)
   leftWall = Wall:new(world, 1, 325, 1, 650)
   rightWall = Wall:new(world, 649, 325, 1, 650)
+
+  world:setGravity(0, 0)
 end
 
 function love.update(dt)
   world:update(dt)
 
   if love.keyboard.isDown("d") then
-    block.body:applyForce(600, 0)
+    block.body:setAngle(block.body:getAngle() + 0.1)
   end
   if love.keyboard.isDown("a") then
-    block.body:applyForce(-600, 0)
+    block.body:setAngle(block.body:getAngle() - 0.1)
   end
   if love.keyboard.isDown("w") then
-    block.body:applyForce(0, -900)
+    local a = block.body:getAngle()
+    block.body:setLinearVelocity(- math.cos(- a - math.pi / 2) * 100, math.sin(- a - math.pi / 2) * 100)
   end
   if love.keyboard.isDown("s") then
-    block.body:applyForce(0, 900)
+    local a = block.body:getAngle()
+    block.body:setLinearVelocity(math.cos(- a - math.pi / 2) * 100, - math.sin(- a - math.pi / 2) * 100)
+  end
+
+  if love.keyboard.isDown("s") == false and love.keyboard.isDown("w") == false then
+    block.body:setLinearVelocity(0, 0)
   end
 end
 
