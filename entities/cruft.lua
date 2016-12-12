3d66c11587deef32cbb24ffa88ccee2ca8be6ccc
Caige Nichols <caigesn@gmail.com>
2016-12-11 22:36:59 -0700

    Make cruft a bit more random.

--- entities/cruft.lua
+++ entities/cruft.lua
@@ -4,7 +4,9 @@ function Cruft:new(x, y)
   newObj = {
     x = x,
     y = y,
-    image = love.graphics.newImage('assets/bin/cruft.png')
+    image = love.graphics.newImage('assets/bin/blarg.png'),
+    offset = math.random(0, 2*math.pi), -- offset in radians
+    scale = math.random(1, 2)
   }
   self.__index = self
   return setmetatable(newObj, self)
@@ -12,7 +14,7 @@ end
 
 function Cruft:draw()
   love.graphics.setColor(255, 255, 255)
-  love.graphics.draw(self.image, self.x, self.y)
+  love.graphics.draw(self.image, self.x, self.y, self.offset, self.scale)
 end
 
 return Cruft
