d6bcfc1117fa3b7ee51e7f63514e9fc881b66e05
Corey Kruger <Corey Kruger>
2016-12-11 21:36:49 -0700

    umm.. dog

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 require('utils/trace')
 
 local Robot = require('entities/robot')
+local Dog = require('entities/dog')
 local Block = require('entities/block')
 local Background = require('entities/background')
 local Ground = require('entities/ground')
@@ -62,6 +63,7 @@ function love.load()
   background:drawCanvas()
 
   rosie = Robot:new(world, 250, 250, 50, 80)
+  dog = Dog:new(world, 350, 350, 50, 50)
   block2 = Block:new(world, 350, 250, 50, 50)
 
   ceiling = Wall:new(world, 325, 1, 650, 1)
@@ -100,7 +102,11 @@ function love.update(dt)
   end
 
   if clutterer:shouldSpawn(#clutter) then
-   clutter[#clutter + 1] = clutterer:spawn()
+    clutter[#clutter + 1] = clutterer:spawn()
+  end
+
+  if dog:spaz() then 
+     dog:move(math.random(-9000, 9000), math.random(-9000, 9000))
   end
 end
 
@@ -122,6 +128,7 @@ function love.draw()
   end
 
   rosie:draw()
+  dog:draw()
   block2:draw()
 
   ceiling:draw()
