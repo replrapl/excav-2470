783d47a86c9f031ec69b1bafb271498d47741da2
Corey Kruger <Corey Kruger>
2016-12-11 18:26:10 -0700

    suckit

--- entities/robot.lua
+++ entities/robot.lua
@@ -23,6 +23,7 @@ end
 function Robot:drive(velocity)
     local angle = self.body:getAngle()
     self.body:setLinearVelocity(math.cos(- angle + math.pi / 2) * velocity, - math.sin(- angle + math.pi / 2) * velocity)
+    self.x, self.y = self.body:getWorldPoints(0, 0)
 end
 
 function Robot:rotate(angle)
@@ -36,6 +37,18 @@ end
 function Robot:draw()
   love.graphics.setColor(255, 255, 255)
   love.graphics.polygon('fill', self.body:getWorldPoints(self.shape:getPoints()))
+end
+
+function Robot:suck(dust)
+    if distanceTo(self.x, self.y, dust.x, dust.y) < 50l then
+        -- trace.print(distanceTo(self.x, self.y, dust.x, dust.y))
+        return true
+    end
+    return false
+end
+
+function distanceTo(x1, y1, x2, y2)
+    return math.sqrt(math.pow(x1 - x2, 2) + math.pow(y1 - y2, 2))
 end
 
 return Robot
